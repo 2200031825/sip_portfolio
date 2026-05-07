114c1dac1037762b91c50dcac8eda95a9eaa23fa
Nithish <2200031825@kluniversity.in>
2026-05-07 18:28:38 +0530

    postman api updates

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.use(express.json());
 const investorRoutes = require("./routes/investorRoutes");
 
 app.use("/api/investor", investorRoutes);
+
 const mutualFundRoutes = require("./routes/mutualRoutes");
 
 app.use(mutualFundRoutes);
